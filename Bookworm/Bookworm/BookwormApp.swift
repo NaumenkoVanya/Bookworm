6c2e729c6ce39a06f80f31f331f2c12fa83c6a86
Vanya <whitewolf3310@icloud.com>
2024-03-11 20:47:59 +0300

    add new project

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Ваня Науменко on 11.03.24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
